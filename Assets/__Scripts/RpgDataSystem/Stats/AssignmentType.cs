de1ed5eb3a1ab76beb2b118819666dcde369ec50
Jibran Ibrahim Syed <jisyed14@gmail.com>
2015-08-13 15:29:15 -0500

    Adding an enum to control how XP is allocated to stats called "AssignmentType"

---
+++ Assets/__Scripts/RpgDataSystem/Stats/AssignmentType.cs
@@ -0,0 +1,10 @@
+﻿namespace SphericalCow
+{
+	public enum AssignmentType 
+	{
+		PointAssigned,		// Uses a global pool that is user-allocated for all stats
+		UseAssigned,		// Uses a stat-local pool per stat that is computer-allocated
+		DefaultAssigned		// Uses whatever the global assignment scheme is (hint: the global scheme cannot be default!)
+	}
+}
+	
