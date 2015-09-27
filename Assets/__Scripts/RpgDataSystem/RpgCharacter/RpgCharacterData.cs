754f6df0aa79d5e2d33c1570b004cfee22b9b734
Jibran Syed <jibranisyed@outlook.com>
2015-09-26 21:28:18 -0500

    Adding a little more interface code to the RpgCharacter test code

--- Assets/__Scripts/RpgDataSystem/RpgCharacter/RpgCharacterData.cs
+++ Assets/__Scripts/RpgDataSystem/RpgCharacter/RpgCharacterData.cs
@@ -26,6 +26,21 @@ namespace SphericalCow
 		//
 
 
+		/// <summary>
+		/// 	Constructor. Will not be called upon deserialization
+		/// </summary>
+		public RpgCharacterData()
+		{
+			this.progressionVariable = ProgressionType.Easy;
+			this.characterName = "Default Player Name";
+			this.basicStats = new List<BasicStatInstance>();
+			this.secondaryStats = new List<SecondaryStatInstance>();
+			this.skillStats = new List<SkillStatInstance>();
+			this.abilities = new List<AbilityInstance>();
+		}
+
+
+
 		//
 		// Setters
 		//
@@ -33,6 +48,11 @@ namespace SphericalCow
 		public void SetCharacterName(string newCharacterName)
 		{
 			this.characterName = newCharacterName;
+		}
+
+		public void SetProgressionVariable(ProgressionType newProgressionVariable)
+		{
+			this.progressionVariable = newProgressionVariable;
 		}
 
 
@@ -47,6 +67,49 @@ namespace SphericalCow
 				return this.characterName;
 			}
 		}
+
+		public ProgressionType ProgressionVariable
+		{
+			get
+			{
+				return this.progressionVariable;
+			}
+		}
+
+		public List<BasicStatInstance> ListOfBasicStats
+		{
+			get
+			{
+				return this.basicStats;
+			}
+		}
+
+		public List<SecondaryStatInstance> ListOfSecondaryStats
+		{
+			get
+			{
+				return this.secondaryStats;
+			}
+		}
+
+		public List<SkillStatInstance> ListOfSkillStats
+		{
+			get
+			{
+				return this.skillStats;
+			}
+		}
+
+		public List<AbilityInstance> ListOfAbilties
+		{
+			get
+			{
+				return this.abilities;
+			}
+		}
+
+
+
 
 	}
 }
