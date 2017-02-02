91951e6458f68416f3d3f9e12eea9803461f45c1
Jibran Ibrahim Syed <jisyed14@gmail.com>
2017-02-02 16:07:39 -0600

    Found a way to make select fields in the Inspector read-only (meaning you can read the value in the Inspector, but you cannot modify it)

---
+++ Assets/__Scripts/ReadOnlyDataAssetSystem/Editor/ReadOnlyDrawer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+// Credit to It3ration: http://answers.unity3d.com/answers/801283/view.html
+
+[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+public class ReadOnlyDrawer : PropertyDrawer
+{
+	public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+	{
+		return EditorGUI.GetPropertyHeight(property, label, true);
+	}
+	
+	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+	{
+		GUI.enabled = false;
+		EditorGUI.PropertyField(position, property, label, true);
+		GUI.enabled = true;
+	}
+}
---
+++ Assets/__Scripts/ReadOnlyDataAssetSystem/ReadOnlyAttribute.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Credit to It3ration: http://answers.unity3d.com/answers/801283/view.html
+
+public class ReadOnlyAttribute : PropertyAttribute
+{
+}
