53758a5fb12592c9ea55e998a50030ed5f6bdac6
Jibran Ibrahim Syed <jisyed14@gmail.com>
2017-02-02 16:08:35 -0600

    Starting to make the AbstractRpgDataType, which will be the basis for other data like stats and abilities

---
+++ Assets/__Scripts/RpgDataSystem/RpgDataTypes/AbstractRpgDataType.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using Guid = System.Guid;
+
+namespace SphericalCow
+{
+	/// <summary>
+	/// 	Represents any read-only RPG data type that has a name, decription, and icon
+	/// </summary>
+	public class AbstractRpgDataType : ScriptableObject 
+	{
+		[SerializeField] private string entityName;
+		[TextArea(3,12)] [SerializeField] private string description;
+		[SerializeField] private Texture icon;
+		[ReadOnly, SerializeField] private SaveableGuid guid;
+		[ReadOnly, SerializeField] private bool isGuidInitialized = false;
+		
+		
+		/// <summary>
+		/// 	Initialized the RPG Data Type. Only runs once.
+		/// </summary>
+		public void Init()
+		{
+			if(!this.isGuidInitialized)
+			{
+				this.guid = new SaveableGuid(true);
+				this.isGuidInitialized = true;
+			}
+		}
+		
+		
+		/// <summary>
+		/// 	The name of this RPG Data Instance
+		/// </summary>
+		public string Name
+		{
+			get
+			{
+				return this.entityName;
+			}
+		}
+		
+		/// <summary>
+		/// 	The description of this RPG Data Instance
+		/// </summary>
+		public string Description
+		{
+			get
+			{
+				return this.description;
+			}
+		}
+		
+		/// <summary>
+		/// 	The icon texture of this RPG Data Instance
+		/// </summary>
+		public Texture Icon
+		{
+			get
+			{
+				return this.icon;
+			}
+		}
+		
+		/// <summary>
+		/// 	The unique GUID of this RPG Data Instance. Used for comparisons.
+		/// </summary>
+		public Guid Id
+		{
+			get
+			{
+				return this.guid.GuidData;
+			}
+		}
+		
+	}
+}
---
+++ Assets/__Scripts/RpgDataSystem/RpgDataTypes/Editor/RpgDataAssetUtility.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace SphericalCow
+{
+	/// <summary>
+	/// 	Used by developers to make file instances of various AbstractRpgDatType 
+	/// 	instances in the Unity project using CustomDataAssetUtility
+	/// </summary>
+	public static class RpgDataAssetUtility 
+	{
+		/// <summary>
+		/// 	Creates a new asset file containing a new instance of #######.
+		/// 	Makes this method accessible from the Unity menu.
+		/// </summary>
+		[MenuItem("Assets/Create/SphericalCow/Test/RPG Data System/Blank RPG Data Type")]
+		public static void CreateBlankRpgDataAsset()
+		{
+			//StatsAndAttributesRegistry registry = StatAssetUtility.FindStatRegistry();
+			AbstractRpgDataType newData = CustomDataAssetUtility.CreateAndReturnDataAsset<AbstractRpgDataType>();
+			newData.Init();
+			//registry.AddBasicStat(newStat);
+		}
+	}
+}
