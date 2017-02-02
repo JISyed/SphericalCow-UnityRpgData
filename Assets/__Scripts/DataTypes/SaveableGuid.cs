e4ca6cb7bb59f3725c16ee8c883aaec1fa76539b
Jibran Ibrahim Syed <jisyed14@gmail.com>
2017-02-02 16:05:52 -0600

    Needed to make a serializable version of the Guid object (because apparently you cannot serialize Guid objects directly). Guids will be used a lot in this new system

---
+++ Assets/__Scripts/DataTypes/SaveableGuid.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System;
+
+/// <summary>
+/// 	A wrapper to C#'s Guid that can be serialized by Unity
+/// </summary>
+[Serializable]
+public class SaveableGuid
+{
+	[NonSerialized] private Guid guid;
+	[SerializeField] private string guidString = "";
+	
+	/// <summary>
+	/// 	Creates an incomplete instance of SavableGuid (nesessary for loading)
+	/// </summary>
+	public SaveableGuid()
+	{
+	}
+	
+	/// <summary>
+	/// 	Specify if you want to make a complete instance of SavableGuid
+	/// </summary>
+	public SaveableGuid(bool doRandomlyChooseGuid)
+	{
+		if (doRandomlyChooseGuid)
+		{
+			this.RandomizeGuid();
+		}
+	}
+	
+	private void RandomizeGuid()
+	{
+		this.guid = Guid.NewGuid();
+		this.UpdateStringRepresentation();
+	}
+	
+	private void UpdateStringRepresentation()
+	{
+		this.guidString = this.guid.ToString();
+	}
+	
+	/// <summary>
+	/// 	Initialize the Guid data from the string representation. The string representation must exist!
+	/// </summary>
+	public void LoadInternalData()
+	{
+		this.guid = new Guid(this.guidString);
+		this.UpdateStringRepresentation();
+	}
+	
+	/// <summary>
+	///  Get the GUID as a String
+	/// </summary>
+	public string GuidString
+	{
+		get
+		{
+			return this.guidString;
+		}
+	}
+	
+	/// <summary>
+	/// 	Get the GUID as a Guid object
+	/// </summary>
+	public Guid GuidData
+	{
+		get
+		{
+			return this.guid;
+		}
+	}
+	
+}
